dcb9cffb8c8f3c98a7e71d0b1cac4b866629bde5
ali_gewei <grey@pku.edu.cn>
2024-10-17 16:48:08 +0800

    add session_id and order_ref to matcher

--- simu_matcher.hpp
+++ simu_matcher.hpp
@@ -11,16 +11,21 @@ enum class Status {
     Canceled
 };
 
+inline uint64_t generate_unique_id(uint32_t x, uint32_t y) {
+    return (static_cast<uint64_t>(x) << 32) | static_cast<uint64_t>(y);
+}
+
 struct BaseSimulator {
     virtual ~BaseSimulator() = default;
-    virtual Status on_order(uint32_t id, uint64_t volume) = 0;
-    virtual Status on_cancel(uint32_t id) = 0;
-    virtual uint64_t get_pending_volume(uint32_t id) const = 0;
+    virtual Status on_order(uint32_t session_id, uint32_t order_ref, uint64_t volume) = 0;
+    virtual Status on_cancel(uint32_t session_id, uint32_t order_ref) = 0;
+    virtual uint64_t get_pending_volume(uint32_t session_id, uint32_t order_ref) const = 0;
 };
 
 struct Simulator : public BaseSimulator {
     std::unordered_map<uint32_t, uint64_t> orders;
-    Status on_order(uint32_t id, uint64_t volume) override {
+    Status on_order(uint32_t session_id, uint32_t order_ref, uint64_t volume) override {
+        auto id = generate_unique_id(session_id, order_ref);
         if (100 <= volume && volume < 400) {
             return Status::Filled;
         } else if (400 <= volume && volume < 1000) {
@@ -35,11 +40,13 @@ struct Simulator : public BaseSimulator {
             return Status::Rejected;
         }
     }
-    Status on_cancel(uint32_t id) override {
+    Status on_cancel(uint32_t session_id, uint32_t order_ref) override {
+        auto id = generate_unique_id(session_id, order_ref);
         return orders.erase(id) ? Status::Canceled : Status::Rejected;
     }
 
-    uint64_t get_pending_volume(uint32_t id) const override {
+    uint64_t get_pending_volume(uint32_t session_id, uint32_t order_ref) const override {
+        auto id = generate_unique_id(session_id, order_ref);
         auto it = orders.find(id);
         return it != orders.end() ? it->second : 0;
         ;
--- simu_matcher_test.cpp
+++ simu_matcher_test.cpp
@@ -25,38 +25,38 @@ int main() {
     std::shared_ptr<BaseSimulator> ptr = std::make_shared<Simulator>();
     Status status;
 
-    status = ptr->on_order(1, 100);
+    int reqId = 0;
+    status = ptr->on_order(1, reqId++, 100);
     assert(status == Status::Filled);
-    status = ptr->on_order(1, 200);
+    status = ptr->on_order(1, reqId++, 200);
     assert(status == Status::Filled);
 
-    status = ptr->on_order(1, 400);
+    status = ptr->on_order(1, reqId++, 400);
     assert(status == Status::PartialFilled);
-    status = ptr->on_order(1, 800);
+    status = ptr->on_order(1, reqId++, 800);
     assert(status == Status::PartialFilled);
 
-    status = ptr->on_order(1, 1000);
+    status = ptr->on_order(1, reqId++, 1000);
     assert(status == Status::Rejected);
-    status = ptr->on_order(1, 1500);
+    status = ptr->on_order(1, reqId++, 1500);
     assert(status == Status::Rejected);
 
-    status = ptr->on_order(1, 2000);
+    status = ptr->on_order(1, reqId++, 2000);
     assert(status == Status::Pending);
-    status = ptr->on_order(1, 10000);
+    status = ptr->on_order(1, reqId++, 10000);
     assert(status == Status::Pending);
 
-    status = ptr->on_order(1, 50);
+    status = ptr->on_order(1, reqId++, 50);
     assert(status == Status::Rejected);
-
-    status = ptr->on_cancel(1);
+    status = ptr->on_cancel(1, 2);
     assert(status == Status::Canceled);
 
     int id = 2;
     for (int i = 0; i < 3; ++i) {
         int vol = i * 200 + 100;
-        status = ptr->on_order(2, vol);
-        printf("send id %d: available vol=%lu/%d, status=%s\n", id, ptr->get_pending_volume(id), vol, repr(status).c_str());
-        status = ptr->on_cancel(2);
-        printf("cancel id %d: available vol=%lu/%d, status=%s\n", id, ptr->get_pending_volume(id), vol, repr(status).c_str());
+        status = ptr->on_order(2, i, vol);
+        printf("send id %d: available vol=%lu/%d, status=%s\n", id, ptr->get_pending_volume(id, i), vol, repr(status).c_str());
+        status = ptr->on_cancel(2, i);
+        printf("cancel id %d: available vol=%lu/%d, status=%s\n", id, ptr->get_pending_volume(id, i), vol, repr(status).c_str());
     }
 }
